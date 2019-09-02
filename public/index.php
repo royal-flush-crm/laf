a25bf05ecdda55b55193e351d53581021ded09f1
liluoao <liluoao@qq.com>
2019-09-02 17:26:51 +0800

    Error handler

--- public/index.php
+++ public/index.php
@@ -3,4 +3,13 @@
 define('APP_ROOT', dirname(__DIR__));
 
 $app = new Yaf\Application(APP_ROOT.'/config/application.ini');
-$app->bootstrap()->run();
+try {
+    $app->bootstrap()->run();
+} catch (Yaf\Exception\LoadFailed\Action $e) {
+    echo '方法输入错误';
+    exit();
+} catch (Yaf\Exception\LoadFailed\Controller $e) {
+    echo '控制器输入错误';
+    exit();
+}
+
