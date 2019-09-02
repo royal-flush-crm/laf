c7e566bd043f16566930df3a0b61193e570f7654
liluoao <liluoao@qq.com>
2019-09-02 15:59:12 +0800

    Add job test

---
+++ app/Controllers/Job.php
@@ -0,0 +1,8 @@
+<?php
+
+class JobController extends BaseController
+{
+    public $actions = [
+        'test' => 'Jobs/Test.php',
+    ];
+}
---
+++ app/Jobs/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+class TestAction extends \Yaf\Action_Abstract
+{
+    public function execute()
+    {
+        echo 'success' . PHP_EOL;
+    }
+}
